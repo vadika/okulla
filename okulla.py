2be69cc95d9c14ab8fb20e0ccdb825bca8c33723
vadik likholetov <vadikas@gmail.com>
2017-07-11 14:49:35 +0300

    generated pdf from web, initial hooks

--- okulla.py
+++ okulla.py
@@ -1,23 +1,56 @@
-from flask import Flask, make_response
+from flask import Flask, make_response, render_template, request, redirect
 from OP_RETURN import *
 import pdfkit
+import uuid
+import hashlib
 
 
 
 testnet = True
 
+
+def trans(key):
+    return hashlib.md5(key.encode("utf-8")).digest()
+
+
 app = Flask(__name__)
 
 @app.route('/')
-def mmmain():
-    pdffile = pdfkit.from_url('http://spb.media/text/teoriya-igr', False)
+def main():
+    return render_template("main.html")
 
-    f = open('/tmp/out.pdf', "wb")
+
+@app.route('/url')
+def url():
+    return render_template("url.html")
+
+
+@app.route('/notarize', methods=['POST'])
+def notarize():
+    url = request.form['url']
+    if not url:
+        return redirect("/url")
+
+    print("url=", url)
+
+    try:
+        pdffile = pdfkit.from_url(url, False)
+    except:
+        return redirect("/url")
+
+    filename = str(uuid.uuid4()
+
+    f = open('store/' + filename + '.pdf', "wb")
     f.write(pdffile)
     f.close()
 
+    return render_template("notarize.html")
+
+
+@app.route("/publish", methods=['POST'])
+def publish():
     # data = "SETERE http://spb.media/text/teoriya-igr http://hole.0xd8.org/check/123456-34568 Petr Ivanov АА"
-    data = 'SETERE  http://spb.media/text/teoriya-igr http://hole.0xd8.org/check/123456'
+    data = str("SETERE  http://spb.media/text/teoriya-igr http://hole.0xd8.org/check/123456")
     result = OP_RETURN_store(data, testnet)
 
     if 'error' in result:
@@ -26,7 +59,7 @@ def mmmain():
         return ("TxIDs:<br>" + "<br>".join(result['txids']) + "<br><br>Ref: " + result[
             'ref'] + "<br><br>Wait a few seconds then check on: http://" +
                 ('testnet.' if testnet else '') + 'coinsecrets.org/' +
-                '<br><iframe id="iframepdf width=800" src="/pdf"></iframe>')
+                '<br><iframe id="iframepdf style="overflow:hidden;height:100%;width:100%" height="100%" width="100%" src="/pdf"></iframe>')
 
 
 @app.route("/pdf")
